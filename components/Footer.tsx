8182efb9f174d92535e23cef29825f3277e3ed4d
Atharv Sadewad <atharvsadewad@gmail.com>
2025-11-23 14:44:51 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,54 +6,23 @@ export default function Footer() {
       </h3>
 
       <div className="flex justify-center gap-8 mb-6">
-
-        {/* Instagram Icon */}
-        <a
-          href="https://www.instagram.com/chandan_patil_nagralkar"
-          target="_blank"
-          className="hover:opacity-80 transition"
-        >
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#b8b8b8"
-            strokeWidth="1.8"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="hover:stroke-yellow-500 transition"
-          >
+        <a href="https://www.instagram.com/chandan_patil_nagralkar" target="_blank" className="hover:opacity-80 transition">
+          <svg width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#b8b8b8" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" className="hover:stroke-yellow-500 transition">
             <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
             <path d="M16 11.37A4 4 0 1 1 12.63 8"></path>
             <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
           </svg>
         </a>
 
-        {/* Facebook Icon */}
-        <a
-          href="https://www.facebook.com/ChandanBaswarajPatilNagralkar"
-          target="_blank"
-          className="hover:opacity-80 transition"
-        >
-          <svg
-            width="32"
-            height="32"
-            fill="none"
-            stroke="#b8b8b8"
-            strokeWidth="1.8"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            viewBox="0 0 24 24"
-            className="hover:stroke-yellow-500 transition"
-          >
+        <a href="https://www.facebook.com/ChandanBaswarajPatilNagralkar" target="_blank" className="hover:opacity-80 transition">
+          <svg width="32" height="32" fill="none" stroke="#b8b8b8" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24" className="hover:stroke-yellow-500 transition">
             <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
           </svg>
         </a>
       </div>
 
       <p className="text-sm text-gray-500">
-        © {new Date().getFullYear()} चंदन बस्वराज पाटील (नागराळकर). All rights reserved.
+        © {new Date().getFullYear()} पाटील चंदन बस्वराज (नागराळकर). All rights reserved.
       </p>
 
       <p className="text-sm text-gray-500 mt-1">
